55d512eafa15bfa31038e299690ff50b649e47b5
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2025-06-19 16:18:08 -0300

    cambios2

--- page_cliente.py
+++ page_cliente.py
@@ -93,22 +93,22 @@ def show_cliente_page(username, cookies):
             "📚 Fotos de mi operación"
         ])
 
-        with tab_photos:
-            # Display 4 photos in a 2x2 layout
-            def show_resized_image(image_path, caption):
-                img = Image.open(image_path)
-                width, height = img.size
-                img = img.resize((width // 2, height // 2))
-                st.image(img, caption=caption, use_container_width=False)
-            st.markdown('---')
-            col1, col2 = st.columns(2)
-            with col1:
-                show_resized_image("foto1.png", "Contenedor")
-                show_resized_image("foto3.png", "Estado de la carga")
-            with col2:
-                show_resized_image("foto2.png", "Precinto")
-                show_resized_image("foto4.png", "Carga en depósito")
-            st.markdown('---')
+    with tab_photos:
+        # Display 4 photos in a 2x2 layout
+        def show_resized_image(image_path, caption):
+            img = Image.open(image_path)
+            width, height = img.size
+            img = img.resize((width // 2, height // 2))
+            st.image(img, caption=caption, use_container_width=False)
+        st.markdown('---')
+        col1, col2 = st.columns(2)
+        with col1:
+            show_resized_image("foto1.png", "Contenedor")
+            show_resized_image("foto3.png", "Estado de la carga")
+        with col2:
+            show_resized_image("foto2.png", "Precinto")
+            show_resized_image("foto4.png", "Carga en depósito")
+        st.markdown('---')
 
-        with tab_review:
-            page_review.show_page_review(username)
+    with tab_review:
+        page_review.show_page_review(username)
